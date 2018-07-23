edbaa643a81980b63ff04d737752b0c6e27b5044
oooooni <41557070+oooooni@users.noreply.github.com>
2018-07-23 11:09:47 +0900

    first commit

---
+++ simpleprogress.go
@@ -0,0 +1,32 @@
+package simpleprogress
+
+import(
+	"fmt"
+	"time"
+)
+
+
+func Simpleprogress(message string) chan<- string {
+	ch := make(chan string)
+	length := len(message) + 3
+	go func() {
+		for {
+			select {
+			case <-ch:
+				fmt.Println("done")
+				return
+			default:
+				fmt.Print("\r")
+				for i:=0; i<length; i++ {
+					fmt.Print(" ")	
+				}
+				fmt.Printf("\r%s", message)
+				for i := 0; i < 3; i++ {
+					fmt.Print(".")
+					time.Sleep(1 * time.Second)
+				}
+			}	
+		}
+	}()
+	return ch	
+}	
